d2995fbd5adc956fce0195f9dca4949beab0c8f8
vldmr42 <kmb14056@yandex.ru>
2025-03-24 12:18:46 +0400

    init commit, base config with css modules lazy loading

---
+++ src/app/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import classes from './App.module.scss';
+import { Link, Outlet } from 'react-router-dom';
+
+export const App = () => {
+    const [count, setCount] = useState(0);
+    const increment = () => setCount((prev) => prev + 1);
+    return (
+        <div>
+            <Link to={'/'}>home</Link>
+            <br />
+            <Link to={'/about'}>about</Link>
+            <br />
+            <Link to={'/shop'}>shop</Link>
+            <h1 className={classes.value}>{count}</h1>
+            <button className={classes.button} onClick={increment}>
+                Inc
+            </button>
+            <Outlet />
+        </div>
+    );
+};
---
+++ src/index.tsx
@@ -0,0 +1,41 @@
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { App } from './app/App';
+import { LazyAbout } from './pages/about/About.lazy';
+import { LazyShop } from './pages/shop/Shop.lazy';
+import { Suspense } from 'react';
+
+const root = document.getElementById('root');
+
+if (!root) {
+    throw new Error('root not found');
+}
+
+const RouterProvider = () => (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />}>
+                <Route
+                    path="/about"
+                    element={
+                        <Suspense fallback="Loading...">
+                            <LazyAbout />
+                        </Suspense>
+                    }
+                />
+                <Route
+                    path="/shop"
+                    element={
+                        <Suspense fallback="Loading...">
+                            <LazyShop />
+                        </Suspense>
+                    }
+                />
+            </Route>
+        </Routes>
+    </BrowserRouter>
+);
+
+const container = createRoot(root);
+
+container.render(<RouterProvider />);
